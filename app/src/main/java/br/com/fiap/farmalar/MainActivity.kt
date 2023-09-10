83424b4bf724cd067e09109d89a5b2d7065e7a93
yanpedroDev <yan.pedro@outlook.com>
2023-09-09 22:20:01 -0300

    Implementacao do titulo da tela inicial

--- app/src/main/java/br/com/fiap/farmalar/MainActivity.kt
+++ app/src/main/java/br/com/fiap/farmalar/MainActivity.kt
@@ -4,13 +4,21 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import br.com.fiap.farmalar.components.HeaderLogo
 import br.com.fiap.farmalar.ui.theme.FarmaLarTheme
+import br.com.fiap.farmalar.ui.theme.Inter
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -31,7 +39,26 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun Main() {
-    Box(modifier = Modifier.fillMaxSize()) {
-        HeaderLogo()
+    Box(
+        modifier = Modifier
+            .fillMaxSize()
+    ) {
+        Column{
+            HeaderLogo()
+
+            Column {
+                Text(
+                    modifier = Modifier
+                        .padding(horizontal = 15.dp)
+                        .fillMaxWidth(),
+                    text = "Olá,\nselecione as opções desejadas",
+                    fontFamily = Inter,
+                    fontWeight = FontWeight.Medium,
+                    fontSize = 16.sp
+                )
+            }
+        }
     }
 }
+
+
