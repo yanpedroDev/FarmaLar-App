6f00f3010060a6bc257e062d108a8691d0687556
yanpedroDev <yan.pedro@outlook.com>
2023-09-09 22:18:59 -0300

    Import da fonte Inter para o projeto

--- app/src/main/java/br/com/fiap/farmalar/ui/theme/Type.kt
+++ app/src/main/java/br/com/fiap/farmalar/ui/theme/Type.kt
@@ -2,18 +2,15 @@ package br.com.fiap.farmalar.ui.theme
 
 import androidx.compose.material3.Typography
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
-import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.unit.sp
+import br.com.fiap.farmalar.R
+
+val Inter = FontFamily(Font(R.font.inter_variable_font_slnt_wght))
 
 // Set of Material typography styles to start with
 val Typography = Typography(
     bodyLarge = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Normal,
-        fontSize = 16.sp,
-        lineHeight = 24.sp,
-        letterSpacing = 0.5.sp
     )
     /* Other default text styles to override
     titleLarge = TextStyle(
