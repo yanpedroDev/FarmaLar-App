9a872ce7982d7b394ded52de1f411a39d03d4ccb
yanpedroDev <yan.pedro@outlook.com>
2023-09-16 12:58:27 -0300

    Adição da Dependência Navigation e mudança do CompileSdk para o mais recente = 34.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 
 android {
     namespace = "br.com.fiap.farmalar"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "br.com.fiap.farmalar"
@@ -59,7 +59,11 @@ dependencies {
     implementation("androidx.compose.ui:ui-graphics")
     implementation("androidx.compose.ui:ui-tooling-preview")
     implementation("androidx.compose.material3:material3")
+
+    // Dependência do LiveData
     implementation("androidx.compose.runtime:runtime-livedata:1.4.3")
+    // Dependência do Navigation
+    implementation("androidx.navigation:navigation-compose:2.7.1")
 
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
