c84f3e36fdaa23e88aae75fa5be1aebf88a44fdd
Ticolino <yan.pedro@outlook.com>
2023-11-22 20:37:40 -0300

    Adicao da coluna Sexo e do botao Criar

--- app/src/main/java/br/com/fiap/farmalar/screens/CadastroDeUsuarios.kt
+++ app/src/main/java/br/com/fiap/farmalar/screens/CadastroDeUsuarios.kt
@@ -1,19 +1,26 @@
 package br.com.fiap.farmalar.screens
 
+import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.RadioButton
+import androidx.compose.material3.RadioButtonDefaults
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.runtime.Composable
@@ -21,6 +28,7 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.res.painterResource
@@ -29,6 +37,7 @@ import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.input.KeyboardCapitalization
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.text.input.PasswordVisualTransformation
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
@@ -52,6 +61,11 @@ fun CadastroDeUsuarios(navController: NavController) {
         mutableStateOf("")
     }
 
+    var sexoSelecionado by remember {
+        mutableStateOf(0)
+    }
+
+
     var inputSenha by remember {
         mutableStateOf("")
     }
@@ -66,35 +80,35 @@ fun CadastroDeUsuarios(navController: NavController) {
         Column(
             modifier = Modifier.fillMaxWidth()
         ) {
-
+            // Logo do FarmaLar
             HeaderLogo()
 
             Spacer(modifier = Modifier.padding(vertical = 4.dp))
 
             Column {
 
-
-                // Titulo Criar Conta
-                Text(
-                    modifier = Modifier
-                        .fillMaxWidth()
-                        .padding(horizontal = 16.dp),
-                    text = "Criar Conta",
-                    style = TextStyle(
-                        fontSize = 32.sp,
-                        fontFamily = Inter,
-                        fontWeight = FontWeight.Bold,
-                        color = colorResource(id = R.color.titulo_cadastro_usuarios)
+                // Titulo Cadastrar Usuário [0]
+                Column {
+                    Text(
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .padding(horizontal = 16.dp),
+                        text = "Cadastrar Usuário",
+                        style = TextStyle(
+                            fontSize = 32.sp,
+                            fontFamily = Inter,
+                            fontWeight = FontWeight.Bold,
+                            color = colorResource(id = R.color.titulo_cadastro_usuarios)
+                        )
                     )
-                )
+                }
 
-
-                // Coluna do Nome Completo
+                // Coluna do Nome Completo [1]
                 Column(
                     Modifier.padding(horizontal = 12.dp, vertical = 8.dp),
                     verticalArrangement = Arrangement.Center
                 )
-                // Input do Nome Completo
+                // Input do Nome Completo [1]
                 {
                     OutlinedTextField(
                         // Valor do Input Nome
@@ -149,12 +163,12 @@ fun CadastroDeUsuarios(navController: NavController) {
                     )
                 }
 
-                // Coluna do Email
+                // Coluna do Email [2]
                 Column(
                     Modifier.padding(horizontal = 12.dp, vertical = 8.dp),
                     verticalArrangement = Arrangement.Center
                 )
-                // Input do Email
+                // Input do Email [2]
                 {
                     OutlinedTextField(
                         // Valor do Input Email
@@ -207,69 +221,61 @@ fun CadastroDeUsuarios(navController: NavController) {
                         })
                 }
 
-                // Coluna do Genero
+                // Coluna do Sexo [3]
                 Column(
-                    Modifier.padding(horizontal = 12.dp, vertical = 8.dp),
-                    verticalArrangement = Arrangement.Center
-                )
-                // Input do Genero
-                {
-                    OutlinedTextField(
-                        // Valor do Input Email
-                        value = inputEmail,
-                        // Variavel de mudança de estado
-                        onValueChange = { inputEmail = it },
-                        shape = RoundedCornerShape(size = 20.dp),
-                        modifier = Modifier.size(325.dp, 60.dp),
-                        keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Email),
-                        singleLine = true,
-                        colors = TextFieldDefaults.outlinedTextFieldColors(
-                            containerColor = colorResource(id = R.color.white),
-                            textColor = colorResource(id = R.color.black),
-                            placeholderColor = colorResource(id = R.color.half_black),
-                            focusedBorderColor = colorResource(id = R.color.orange_text),
-                            unfocusedBorderColor = colorResource(id = R.color.borda_inputs_cadastro),
-                            unfocusedLeadingIconColor = colorResource(id = R.color.color_icons),
-                            focusedLeadingIconColor = colorResource(id = R.color.orange_text),
-                            unfocusedLabelColor = colorResource(id = R.color.half_black),
-                            focusedLabelColor = colorResource(id = R.color.orange_text),
-
+                ) {
+                    // Input do Sexo [3]
+                    Row(
+                        verticalAlignment = Alignment.CenterVertically
+                    ) {
+                        RadioButton(
+                            selected = sexoSelecionado == 0,
+                            onClick = { sexoSelecionado = 0 },
+                            modifier = Modifier.padding(16.dp),
+                            colors = RadioButtonDefaults.colors(
+                                colorResource(
+                                    id = R.color.orange_button
+                                )
                             ),
-                        label = {
+                        )
+                        Text(
+                            text = "Masculino",
+                            modifier = Modifier.offset((-20).dp),
+                            fontFamily = Inter,
+                            fontWeight = FontWeight.Medium,
+                            fontSize = 16.sp,
+                            color = colorResource(id = R.color.titulo_cadastro_usuarios)
+                        )
+                        Row(
+                            verticalAlignment = Alignment.CenterVertically
+                        ) {
+                            RadioButton(
+                                selected = sexoSelecionado == 1,
+                                onClick = { sexoSelecionado = 1 },
+                                modifier = Modifier.padding(16.dp),
+                                colors = RadioButtonDefaults.colors(
+                                    colorResource(
+                                        id = R.color.orange_button
+                                    )
+                                ),
+                            )
                             Text(
-                                modifier = Modifier.padding(horizontal = 2.dp),
-                                text = "Gênero",
-                                fontSize = 18.sp,
+                                text = "Feminino",
+                                modifier = Modifier.offset((-20).dp),
                                 fontFamily = Inter,
                                 fontWeight = FontWeight.Medium,
+                                fontSize = 16.sp,
+                                color = colorResource(id = R.color.titulo_cadastro_usuarios)
                             )
-                        },
-                        placeholder = {
-                            Text(
-                                modifier = Modifier.padding(horizontal = 4.dp),
-                                text = "",
-                                fontSize = 14.sp,
-                                fontFamily = Inter,
-                                fontWeight = FontWeight.SemiBold,
-                                color = colorResource(id = R.color.half_black)
-                            )
-                        },
-                        leadingIcon = {
-                            Icon(
-                                modifier = Modifier
-                                    .offset(8.dp)
-                                    .size(30.dp, 30.dp),
-                                painter = painterResource(id = R.drawable.input_gender),
-                                contentDescription = "Ícone de Email"
-                            )
-                        })
+                        }
+                    }
                 }
 
-                // Coluna da Data de Nascimento
+                // Coluna da Data de Nascimento [3]
                 Column(
                     Modifier.padding(horizontal = 12.dp, vertical = 8.dp),
                     verticalArrangement = Arrangement.Center
-                // Input da Data de Nascimento
+                    // Input da Data de Nascimento [3]
                 ) {
                     OutlinedTextField(
                         // Valor do Input Data de Nascimento
@@ -278,7 +284,7 @@ fun CadastroDeUsuarios(navController: NavController) {
                         onValueChange = { inputData = it },
                         shape = RoundedCornerShape(size = 20.dp),
                         modifier = Modifier.size(325.dp, 60.dp),
-                        keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Text),
+                        keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number),
                         singleLine = true,
                         colors = TextFieldDefaults.outlinedTextFieldColors(
                             containerColor = colorResource(id = R.color.white),
@@ -303,7 +309,7 @@ fun CadastroDeUsuarios(navController: NavController) {
                         placeholder = {
                             Text(
                                 modifier = Modifier.padding(horizontal = 4.dp),
-                                text = "Digite sua data de nascimento",
+                                text = "Digite no formato: DD/MM/AAAA",
                                 fontSize = 14.sp,
                                 fontFamily = Inter,
                                 fontWeight = FontWeight.SemiBold,
@@ -322,11 +328,11 @@ fun CadastroDeUsuarios(navController: NavController) {
                     )
                 }
 
-                // Coluna da Senha
+                // Coluna da Senha [4]
                 Column(
                     Modifier.padding(horizontal = 12.dp, vertical = 8.dp),
                     verticalArrangement = Arrangement.Center
-                    // Input da Senha
+                    // Input da Senha [4]
                 ) {
                     OutlinedTextField(
                         // Valor do Input Senha
@@ -336,8 +342,9 @@ fun CadastroDeUsuarios(navController: NavController) {
                         shape = RoundedCornerShape(size = 20.dp),
                         modifier = Modifier.size(325.dp, 60.dp),
                         keyboardOptions = KeyboardOptions(
-                            keyboardType = KeyboardType.NumberPassword),
-                            visualTransformation = PasswordVisualTransformation(),
+                            keyboardType = KeyboardType.NumberPassword
+                        ),
+                        visualTransformation = PasswordVisualTransformation(),
                         singleLine = true,
                         colors = TextFieldDefaults.outlinedTextFieldColors(
                             containerColor = colorResource(id = R.color.white),
@@ -381,11 +388,11 @@ fun CadastroDeUsuarios(navController: NavController) {
                     )
                 }
 
-                // Coluna de Confirmar Senha
+                // Coluna de Confirmar Senha [5]
                 Column(
                     Modifier.padding(horizontal = 12.dp, vertical = 8.dp),
                     verticalArrangement = Arrangement.Center
-                    // Input do Confirmar Senha
+                    // Input do Confirmar Senha [5]
                 ) {
                     OutlinedTextField(
                         // Valor do Input Senha
@@ -395,7 +402,8 @@ fun CadastroDeUsuarios(navController: NavController) {
                         shape = RoundedCornerShape(size = 20.dp),
                         modifier = Modifier.size(325.dp, 60.dp),
                         keyboardOptions = KeyboardOptions(
-                            keyboardType = KeyboardType.NumberPassword),
+                            keyboardType = KeyboardType.NumberPassword
+                        ),
                         visualTransformation = PasswordVisualTransformation(),
                         singleLine = true,
                         colors = TextFieldDefaults.outlinedTextFieldColors(
@@ -440,6 +448,38 @@ fun CadastroDeUsuarios(navController: NavController) {
                     )
                 }
 
+                Spacer(modifier = Modifier.height(35.dp))
+
+                // Coluna do Botao Criar
+                Column(
+                    horizontalAlignment = Alignment.CenterHorizontally,
+                    verticalArrangement = Arrangement.Center,
+                    modifier = Modifier
+                        .fillMaxWidth()
+                ) {
+                    Button(
+                        modifier = Modifier.size(325.dp, 55.dp),
+                        onClick = {
+                            navController.navigate("login")
+                        },
+                        colors = ButtonDefaults.buttonColors(
+                            colorResource
+                                (id = R.color.orange_button)
+                        ),
+                        elevation = ButtonDefaults.elevatedButtonElevation(6.dp),
+                        border = BorderStroke(2.dp, colorResource(id = R.color.orange_text))
+                    ) {
+                        Text(
+                            modifier = Modifier.fillMaxWidth(),
+                            text = "Criar",
+                            textAlign = TextAlign.Center,
+                            fontFamily = Inter,
+                            fontSize = 28.sp,
+                            fontWeight = FontWeight.Bold,
+                            color = colorResource(id = R.color.white)
+                        )
+                    }
+                }
             }
         }
     }
